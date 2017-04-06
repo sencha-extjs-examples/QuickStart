5a1bddc4a9ace6f445edb9d5bea3070d0c7f5cab
gregbarry <greg.barry@extjs.com>
2017-04-06 15:27:25 -0500

    Update Employees.js

--- app/store/Employees.js
+++ app/store/Employees.js
@@ -5,5 +5,22 @@ Ext.define('QuickStart.store.Employees', {
     proxy: {
         type: 'ajax',
         url: 'data/data.json'
+    },
+
+    listeners: {
+
+        update: function(store, record , operation , modifiedFieldNames) {
+            if (!modifiedFieldNames) {
+                return;
+            }
+
+            // Ensure that select field is being set to a value, not the entire record
+            var modField = modifiedFieldNames.toString(),
+                mod = record.get(modField);
+
+            if (mod && mod.isModel) {
+                record.set(modField, mod.get('text'));
+            }
+        }
     }
 });
